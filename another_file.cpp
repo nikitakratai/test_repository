c0f5c7d2a8f3af2b720bf64fee597593fc731ce2
nikitakratai <159303435+nikitakratai@users.noreply.github.com>
2024-02-29 19:28:22 +0700

    Update another_file.cpp

--- another_file.cpp
+++ another_file.cpp
@@ -1,4 +1,11 @@
 #include math,stdio
+#include <stdint.h>
+
+typedef int32_t i32_t;
+typedef int_fast32_t i32fast_t;
+typedef int_least32_t i32least_t;
+
+
 print('pronto!')
 print('=_)')
 print('5+2=',%f,5+2)
@@ -9,8 +16,10 @@ for (i=0,i++,i==10):
 end;
 
 if (2+3<7):
-  print(2+3<7)
+  print('2+3<7')
 else
 {
   print('2+3>=7')
   }
+
+return 0
