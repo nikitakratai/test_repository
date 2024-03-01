b8c97dc8f2eae8b9af644985b4a5777c7a3cf6cc
nikitakratai <159303435+nikitakratai@users.noreply.github.com>
2024-03-01 20:25:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,16 @@ else
 from math import asin
 x = asin(0.53)
 print('Asin(0.53) = ', asin(0.53))
+
+
+stri = '*' * 50 + '/n'
+for a in range(1, 5, 1)
+  print(stri)
+
+print('Let us show all even and odd numbers in range from 1 to 9.')
+
+for i in range(1, 10, 1)
+  if (mod(i,2)==1)==True
+    print(i,' is an odd number')
+  else
+    print(i, ' is an even number')
