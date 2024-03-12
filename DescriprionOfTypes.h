56f291a897cd27813879114c80405bbaf456fe2f
nikitakratai <159303435+nikitakratai@users.noreply.github.com>
2024-03-12 13:10:36 +0700

    Update DescriprionOfTypes.h

--- DescriprionOfTypes.h
+++ DescriprionOfTypes.h
@@ -1,14 +1,20 @@
 // it's a file which contains description of add used types;
 
-struct wheel
+struct wheel:
 {
  diameter: real;
  isVoronenied: bool;
 }
 
-struct t_car
+struct t_car:
 {
  weight, length, height, width: double;
  color: TColor;
  w1, w2, w3, w4: wheel;
+}
+
+struct cortage:
+{
+ num: integer;
+ cars:t_car[num];
 }
