c87a1b05eab77aa57fd9ebd2790af39557643ddd
nikitakratai <159303435+nikitakratai@users.noreply.github.com>
2024-05-18 08:31:53 +0700

    Update definitions.py

--- definitions.py
+++ definitions.py
@@ -5,3 +5,7 @@ fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
 fruits.count('apple')
 fruits.count('tangerine')
 fruits.index('banana')
+
+%% enumerate() function.
+for i, v in enumerate(['first', 'second', 'third', 'fourth']):
+  print(i, v)
