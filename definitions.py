e5887552a54a2876e5c1933afed776d622037a45
nikitakratai <159303435+nikitakratai@users.noreply.github.com>
2024-05-26 12:32:49 +0700

    Update definitions.py

--- definitions.py
+++ definitions.py
@@ -9,3 +9,10 @@ fruits.index('banana')
 %% enumerate() function.
 for i, v in enumerate(['first', 'second', 'third', 'fourth']):
   print(i, v)
+
+%%
+for num in range(2, 10):
+  if num % 2 == 0:
+  print("Found an even number", num)
+  continue
+print("Found a number", num)
